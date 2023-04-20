d8de0e04d19ea2c05cfa5d9f2df643a958c9f527
ShinBlake <blakesungjin@gmail.com>
2023-04-20 01:31:01 -0500

    tested

--- react/src/setupProxy.js
+++ react/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app){
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'vandy-tracker-api-dev2.us-east-2.elasticbeanstalk.com',
+            target: 'http://backend:5000',
             changeOrigin: true,
         })
     );
